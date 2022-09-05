f061b1c988727a8e522b361ac5b0e30c30a8ee17
Diego Lirio <83074806+ddamacena@users.noreply.github.com>
2022-09-05 10:33:02 -0300

    purchase order

---
+++ purchase-order/purchase-order-process/src/main/kotlin/com/liriotech/purchaseorderprocess/PurchaseOrderProcessApplication.kt
@@ -0,0 +1,11 @@
+package com.liriotech.purchaseorderprocess
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PurchaseOrderProcessApplication
+
+fun main(args: Array<String>) {
+	runApplication<PurchaseOrderProcessApplication>(*args)
+}
---
+++ purchase-order/purchase-order/src/main/kotlin/com/liriotech/purchaseorder/PurchaseOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.liriotech.purchaseorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PurchaseOrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<PurchaseOrderApplication>(*args)
+}
